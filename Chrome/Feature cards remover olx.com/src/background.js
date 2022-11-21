0893b6e20fec9418f1176e9156d7d4f564cb6c45
Shahid Saddique <msaddique@Powersoft19.com>
2022-11-21 13:11:47 +0500

    working in one click function.

--- Chrome/Feature cards remover olx.com/src/background.js
+++ Chrome/Feature cards remover olx.com/src/background.js
@@ -1,23 +1,52 @@
-function reddenPage() {
-  //document.body.style.backgroundColor = 'blue';
+//Change the background color
+function changebackground() {
   document.getElementsByTagName("header")[1].style.background = "lightblue";
-  var cards = document.getElementsByTagName("ul")[1].getElementsByTagName("li");
-  for (let index = 0; index < cards.length; index++) {
-    var text = cards[index].innerText;
-    if (text.startsWith("FEATURED") || text.startsWith('Want to see your stuff here')) {
-      cards[index].remove();
+}
+//Select main cards container
+function selectmaincontainer() {
+  var featureText = "FEATURED";
+  var wantToSeeYour = "Want to see your stuff here";
+  var mainCardUnOrderList = 1;
+  var listTags = document.getElementsByTagName("ul");
+  for (let index = 0; index < listTags.length; index++) {
+    console.log(index + ": " + listTags[index].innerText);
+    if (listTags[index].innerText.startsWith(featureText))
+      mainCardUnOrderList = index;
+    console.log(index + ": Start with required text");
+  }
+  var cards = document.getElementsByTagName("ul")[mainCardUnOrderList].getElementsByTagName("li");
+  removeCards(cards);
+  console.log("End Operation!");
+}
+
+function removeCards(cardsCollection) {
+  document.getElementsByTagName("header")[1].style.background = "red";
+  for (let index = 0; index < cardsCollection.length; index++) {
+    var text = cardsCollection[index].innerText;
+    if (text.startsWith(featureText) || text.startsWith(wantToSeeYour)) {
+      cardsCollection[index].remove();
+      continue;
+    }
+    var text = cardsCollection[index].innerText;
+    if (text.startsWith(featureText) || text.startsWith(wantToSeeYour)) {
+      cardsCollection[index].remove();
+      continue;
+    }
+    var text = cardsCollection[index].innerText;
+    if (text.startsWith(featureText) || text.startsWith(wantToSeeYour)) {
+      cardsCollection[index].remove();
       continue;
     }
     console.log(text);
   }
-  console.log("Created tab ");
 }
 
 chrome.action.onClicked.addListener((tab) => {
-  if ((!tab.url.includes("chrome://"))&&(tab.url.includes("olx.com"))) {
+  if (!tab.url.includes("chrome://") && tab.url.includes("olx.com")) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: reddenPage,
+      function: selectmaincontainer,
+      function: changebackground,
     });
   }
 });
