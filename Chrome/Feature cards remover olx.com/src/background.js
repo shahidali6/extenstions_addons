429dd1dd7835f2eda42aa92a064e52fe5a8cd083
Shahid Saddique <shahidali6@gmail.com>
2023-04-30 01:20:08 +0500

    1. remove feature cards from all ul tags 2. main loop to remove all feature cards is equal to 10

--- Chrome/Feature cards remover olx.com/src/background.js
+++ Chrome/Feature cards remover olx.com/src/background.js
@@ -1,23 +1,45 @@
-function reddenPage() {
+function feature_card_remover() {
+  //fields
+  let total_cards = 0
+  let total_feature_card = 0
   //document.body.style.backgroundColor = 'blue';
-  document.getElementsByTagName("header")[1].style.background = "lightblue";
-  var cards = document.getElementsByTagName("ul")[1].getElementsByTagName("li");
-  for (let index = 0; index < cards.length; index++) {
-    var text = cards[index].innerText;
-    if (text.startsWith("FEATURED") || text.startsWith('Want to see your stuff here')) {
-      cards[index].remove();
-      continue;
-    }
-    console.log(text);
+  //Change the color of the background to check the extension is working fine.
+  document.getElementsByTagName('header')[1].style.background = 'lightblue'
+
+  var totalLists = document.getElementsByTagName('ul')
+  for (let listIndex = 0; listIndex < totalLists.length; listIndex++) {
+    //Print the name of all lists present on the page
+    console.log(totalLists[listIndex])
   }
-  console.log("Created tab ");
+
+  //loop for exact 10 times
+  for (let i = 0; i < 10; i++) {
+    //trivers on all list using li tag
+    for (let j = 0; j < totalLists.length; j++) {
+      var cards = document
+        .getElementsByTagName('ul')
+        [j].getElementsByTagName('li')
+      for (let k = 0; k < cards.length; k++) {
+        var text = cards[k].innerText
+        if (
+          text.startsWith('FEATURED') ||
+          text.startsWith('Want to see your stuff here')
+        ) {
+          cards[k].remove()
+          continue
+        }
+        console.log(text)
+      }
+    }
+  }
+  console.log('Created tab ')
 }
 
 chrome.action.onClicked.addListener((tab) => {
-  if ((!tab.url.includes("chrome://"))&&(tab.url.includes("olx.com"))) {
+  if (!tab.url.includes('chrome://') && tab.url.includes('olx.com')) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: reddenPage,
-    });
+      function: feature_card_remover,
+    })
   }
-});
+})
