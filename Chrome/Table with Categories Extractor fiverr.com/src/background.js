5e25c560714b1f00b9376ff87149d1a8ec9c4f0f
Shahid Saddique <shahidali6@gmail.com>
2023-04-30 20:07:59 +0500

    1. date data added 2. remove dead code

--- Chrome/Table with Categories Extractor fiverr.com/src/background.js
+++ Chrome/Table with Categories Extractor fiverr.com/src/background.js
@@ -7,6 +7,7 @@ function feature_card_remover() {
   let sub_category = 'NA'
   let service_type = 'NA'
   let row_builder = ''
+  let date = 'NA'
 
   //Get the list of categories
   var categories = document.getElementsByClassName('W46fKfn cK9KPQl XlivBII')
@@ -22,66 +23,46 @@ function feature_card_remover() {
         break
       case 2:
         service_type = categories[i].textContent
+
         break
     }
-    //Print the name of all categories present on the page
-    //console.log(categories[i])
   }
 
-  var table = document.getElementsByTagName('table')
-  //var table_header_row_cell = table.row.getElementsByTagName('th')
+  var getdate = document.getElementsByClassName('co-grey-800 tbody-6')
 
-  // for (let i = 0, row; row = table.rows[i]; i++) {
-  //   for (let j = 0, col; col = row.cells[j]; j++) {
-  //     row_builder += col + ','
-  //   }
-  //   console.log(row_builder)
-  //   row_builder = ''
-  // }
+  for (let i = 0; i < getdate.length; i++) {
+    date = getdate[i].textContent
+  }
 
-  // for (let row of table.rows) {
-  //   for (let cell of row.cells) {
-  //     row_builder += cell + ','
-  //   }
-  //   console.log(row_builder)
-  //   row_builder = ''
-  // }
-
-  // let i = 0
-  // while ((row = table.rows[i++])) {}
   var cell = document.getElementsByTagName('td')
-  console.log(cell.length)
+
+  let hash = '#'
+  let empty = ''
 
   for (let i = 0, j = 0; i < cell.length; i++) {
-    row_builder = row_builder.concat(cell[i].textContent + ',')
-    //row_builder += cell[i].textContent + ','
+    row_builder = row_builder.concat(
+      cell[i].textContent.replace(hash, empty) + ',',
+    )
+
     if (++j > 5) {
       console.log(
-        category + ',' + sub_category + ',' + service_type + ',' + row_builder.length,
+        category +
+          ',' +
+          sub_category +
+          ',' +
+          service_type +
+          ',' +
+          row_builder +
+          date,
       )
       row_builder = ''
       j = 0
     }
   }
 
-  // for (let i = 0; i < table_header_row_cell.length; i++) {
-  //   row_builder += table_header_row_cell[i] + ','
-  // }
   //Print the name of all lists present on the page
   console.log(row_builder)
   row_builder = ''
-
-  // var table_body = table.getElementsByTagName('tbody')
-  // var table_rows = table_body.getElementsByTagName('tr')
-
-  // for (let i = 0; i < table_rows.length; i++) {
-  //   var row_cells = table_rows[i].getElementsByTagName('td')
-  //   for (let j = 0; j < row_cells.length; j++) {
-  //     row_builder += table_header_row_cell[j] + ','
-  //   }
-  //   console.log(row_builder)
-  //   row_builder = ''
-  // }
 }
 
 chrome.action.onClicked.addListener((tab) => {
