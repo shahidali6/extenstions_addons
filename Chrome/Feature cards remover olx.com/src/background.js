e2a1632f731091a8fde5f07c04c98c24437f3713
Shahid Saddique <shahidali6@gmail.com>
2023-04-30 13:12:23 +0500

    1. skipping feature card fixed.

--- Chrome/Feature cards remover olx.com/src/background.js
+++ Chrome/Feature cards remover olx.com/src/background.js
@@ -2,9 +2,10 @@ function feature_card_remover() {
   //fields
   let total_cards = 0
   let total_feature_card = 0
+  let remainig_cards = 0
   //document.body.style.backgroundColor = 'blue';
   //Change the color of the background to check the extension is working fine.
-  document.getElementsByTagName('header')[1].style.background = 'lightblue'
+  document.getElementsByTagName('header')[0].style.background = 'lightblue'
 
   var totalLists = document.getElementsByTagName('ul')
   for (let listIndex = 0; listIndex < totalLists.length; listIndex++) {
@@ -12,27 +13,25 @@ function feature_card_remover() {
     console.log(totalLists[listIndex])
   }
 
-  //loop for exact 10 times
-  for (let i = 0; i < 10; i++) {
-    //trivers on all list using li tag
-    for (let j = 0; j < totalLists.length; j++) {
-      var cards = document
-        .getElementsByTagName('ul')
-        [j].getElementsByTagName('li')
-      for (let k = 0; k < cards.length; k++) {
-        var text = cards[k].innerText
-        if (
-          text.startsWith('FEATURED') ||
-          text.startsWith('Want to see your stuff here')
-        ) {
-          cards[k].remove()
-          continue
-        }
-        console.log(text)
+  //trivers on all list using li tag
+  for (let j = 0; j < totalLists.length; j++) {
+    var cards = document
+      .getElementsByTagName('ul')
+      [j].getElementsByTagName('li')
+    for (let k = 0; k < cards.length; k++) {
+      var text = cards[k].innerText
+      if (
+        text.startsWith('FEATURED') ||
+        text.startsWith('Want to see your stuff here')
+      ) {
+        cards[k].remove()
+        k--
+        total_feature_card++
+        continue
       }
     }
   }
-  console.log('Created tab ')
+  console.log('Total Feature Cards Removed: ' + total_feature_card)
 }
 
 chrome.action.onClicked.addListener((tab) => {
