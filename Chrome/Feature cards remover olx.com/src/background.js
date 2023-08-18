2b393770ebb63724da1dd169db6c53229c179ced
Shahid Saddique <msaddique@Powersoft19.com>
2023-08-19 04:32:49 +0500

    card hide instead of removed. find li instead of ul

--- Chrome/Feature cards remover olx.com/src/background.js
+++ Chrome/Feature cards remover olx.com/src/background.js
@@ -1,44 +1,50 @@
 function feature_card_remover() {
   //fields
-  let total_cards = 0
-  let total_feature_card = 0
-  let remainig_cards = 0
+  let total_cards = 0;
+  let total_feature_card = 0;
+  let remainig_cards = 0;
   //document.body.style.backgroundColor = 'blue';
   //Change the color of the background to check the extension is working fine.
-  document.getElementsByTagName('header')[0].style.background = 'lightblue'
+  document.getElementsByTagName("header")[0].style.background = "lightblue";
 
-  var totalLists = document.getElementsByTagName('ul')
-  for (let listIndex = 0; listIndex < totalLists.length; listIndex++) {
-    //Print the name of all lists present on the page
-    console.log(totalLists[listIndex])
+  // for (let index = 0; index < 3; index++) {
+  //   setTimeout(console.log(index + 1 + ": itration"), 15000);
+
+  //   var buttons = document.getElementsByTagName("button");
+
+  //   for (let index = 0; index < buttons.length; index++) {
+  //     var text = buttons[index].innerText.trim();
+  //     if (text.toLowerCase().startsWith("load more")) {
+  //       buttons[index].click();
+  //       console.log("Button cliked");
+  //     }
+  //   }
+  // }
+
+  //all words or text that need to avoid
+  const hate_words = ["featured", "want to see your stuff here"];
+
+  //Get all 'li' items
+  var cards = document.getElementsByTagName("li");
+
+  let removeCards = 0;
+
+  //Trivers all the 'li' items and manupolate as required
+  for (let index = 0; index < cards.length; index++) {
+    var text = cards[index].innerText.trim();
+    if (hate_words.some((word) => text.toLowerCase().startsWith(word)))
+      cards[index].style.display = "none";
+    removeCards++;
   }
 
-  //trivers on all list using li tag
-  for (let j = 0; j < totalLists.length; j++) {
-    var cards = document
-      .getElementsByTagName('ul')
-      [j].getElementsByTagName('li')
-    for (let k = 0; k < cards.length; k++) {
-      var text = cards[k].innerText
-      if (
-        text.startsWith('FEATURED') ||
-        text.startsWith('Want to see your stuff here')
-      ) {
-        cards[k].remove()
-        k--
-        total_feature_card++
-        continue
-      }
-    }
-  }
-  console.log('Total Feature Cards Removed: ' + total_feature_card)
+  console.log("Total Feature Cards hidden: " + removeCards);
 }
 
 chrome.action.onClicked.addListener((tab) => {
-  if (!tab.url.includes('chrome://') && tab.url.includes('olx.com')) {
+  if (!tab.url.includes("chrome://") && tab.url.includes("olx.com.pk")) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: feature_card_remover,
-    })
+    });
   }
-})
+});
